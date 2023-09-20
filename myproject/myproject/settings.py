773cea4e3fa81fba2509d86e66bc44fd2a0647d3
Zelendewagen <Zelendewagen@gmail.com>
2023-09-21 06:04:43 +1000

    HW 6.1 init

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'drtn%default',
         'USER': 'drtn',
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'PASSWORD': '130375',
         'HOST': 'drtn.mysql.pythonanywhere-services.com',
         'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
